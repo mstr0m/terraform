1416cb768e8632d2491c770bdf74fd541acbd17d
mstr0m <61348380+mstr0m@users.noreply.github.com>
2022-02-26 17:45:40 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -9,13 +9,13 @@ resource "aws_security_group" "webserver_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
   
