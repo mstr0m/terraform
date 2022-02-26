e31e7962f094ac1c7f49f5c3e5f66b249ff38dc6
Mike <mikestromm@gmail.com>
2022-02-26 13:43:26 +0000

    xclude cred

--- test.tf
+++ test.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  access_key = "ASIAUNI52FBFLPDHCO7Z"
-  secret_key = "pVgykMvDDunZdZUdMIisULkA5MuDC9SfwQ9h5TGZ"
-  token = "FwoGZXIvYXdzEHYaDOSDqZ9qhGYAffmQvCK1AXJPiFLzyd+4vrAIXqHbgXvRYM/ft4UEgH/H2xTo1KI/+0bBgRBoq2DZDnZwSP0Seu47KqNukw8iEN4rCZ9a/kavcS29tWb9vgxPCawdowy8oKNoem24URHRv7tpwC1S8VQKRHrGJLASu+kjBvM53MVyP171z5LofLJot/6j7GKArdJqc+WGm0PpBh6SEwBmaNaf+djXtH+MFzEv4BmAuuhzo71aXKcombZlMRDTxMMeEGh1M9EovrXokAYyLYCvhoIUUZjkU58hNSFhrqxAcsPq4vxheefgL5ja4wqO0RPdLFx4iuH4XkcEnQ=="
-  region = "us-east-1"
-}
-
 variable "AWS_REGION" {
   type = string
   default = "us-east-1"
