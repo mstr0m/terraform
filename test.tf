379cac921dfec88ea7fbff55a95d81f253a9efec
mstr0m <61348380+mstr0m@users.noreply.github.com>
2022-02-26 17:42:23 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -3,7 +3,31 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 
+resource "aws_security_group" "webserver_sg" {
+  name = "Port 22"
+  ingerss {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = "0.0.0.0/0"
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }
+}
+  
 resource "aws_instance" "simplilearn" {
   ami = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
+  key_name = "aws"
+  vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+  tags = {
+     Name = "Ubuntu"
+  }
+  output "my-public-ip" {
+    value = aws_instance.public_ip
+  }
 }
